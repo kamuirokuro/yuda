c22aefdc92281fd501eae4e594a1463daca5de64
kamuirokuro <79147759+kamuirokuro@users.noreply.github.com>
2021-02-21 03:53:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./yuda -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DDf4DpCBPQaCxAVs4yuB4o5Km2vhkxG7yW -p c=DOGE -t 4
+./yuda -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DDf4DpCBPQaCxAVs4yuB4o5Km2vhkxG7yW -p c=DOGE -t 1
 
 
 done
